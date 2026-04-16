377dfca434923d920677864a12b323cdaa3c6d4a
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-04-16 19:00:56 -0400

    feat: define ChatInboundPayloads and ChatOutboundPayloads interfaces for message handling

--- src/modules/chat/domain/events/ChatEvents.ts
+++ src/modules/chat/domain/events/ChatEvents.ts
@@ -1,3 +1,8 @@
+import { CreateMessageDto } from "../../application/dtos/CreateMessage.dto";
+import { MessageParamsDto } from "../../application/dtos/MessageParams.dto";
+import { UpdateMessageDto } from "../../application/dtos/UpdateMessage.dto";
+import { ChatMessage } from "../entities/ChatMessage.entity";
+
 // Eventos que el servidor recibe del cliente
 export enum ChatInboundEvent {
   SEND_MESSAGE = "chat:message:send",
@@ -11,4 +16,34 @@ export enum ChatOutboundEvent {
   MESSAGE_UPDATED = "chat:message:updated",
   MESSAGE_DELETED = "chat:message:deleted",
   ERROR = "chat:error",
+}
+
+// Lo que el cliente DEBE enviar
+export interface ChatInboundPayloads {
+  [ChatInboundEvent.SEND_MESSAGE]: CreateMessageDto;
+  [ChatInboundEvent.UPDATE_MESSAGE]: {
+    params: MessageParamsDto;
+    body: UpdateMessageDto;
+    currentUsername: string;
+  };
+  [ChatInboundEvent.DELETE_MESSAGE]: {
+    params: MessageParamsDto;
+    currentUsername: string;
+  };
+}
+
+// Lo que el cliente VA A recibir
+export interface ChatOutboundPayloads {
+  [ChatOutboundEvent.MESSAGE_CREATED]: ChatMessage;
+  [ChatOutboundEvent.MESSAGE_UPDATED]: ChatMessage;
+  [ChatOutboundEvent.MESSAGE_DELETED]: { id: string };
+  [ChatOutboundEvent.ERROR]: { message: string; code: string };
+}
+
+// Realmente no deberia ir aqui,
+// pero como solo es un gateway en toda la app pues lo dejamos aqui xd
+export interface WsResponse<T = void> {
+  status: "ok" | "error";
+  data?: T;
+  message?: string;
 }
