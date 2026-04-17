1915f920dc34acc10c58e868d1fe5274985e11b2
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-04-16 20:57:00 -0400

    fix: update API documentation details in app.ts and comment out unused fs import

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import fs from "node:fs";
+// import fs from "node:fs";
 import { FastifyKit } from "@neunoro/fastify-kit";
 import { AppModule } from "./app.module";
 import { envSchema } from "./config/env.schema";
@@ -30,18 +30,19 @@ export async function buildApp() {
     // Configuración de Documentación (Swagger + Scalar)
     // Generará automáticamente la ruta /docs
     swagger: {
-      title: "Books API",
-      description: "API de alto rendimiento construida con Neucore Framework",
+      title: "Chat API",
+      description:
+        "API de alto rendimiento construida con FastifyKit (desarrollado por Angel)",
       version: "1.0.0",
     },
 
     // Opciones de bajo nivel para la instancia de Fastify (SSL, HTTP2, etc.)
     fastifyOptions: {
       // http2: true,
-      https: {
-        key: fs.readFileSync("./localhost+2-key.pem"),
-        cert: fs.readFileSync("./localhost+2.pem"),
-      },
+      // https: {
+      //   key: fs.readFileSync("./localhost+2-key.pem"),
+      //   cert: fs.readFileSync("./localhost+2.pem"),
+      // },
       logger: false, // El framework usa su propio sistema de logs
     },
   });
